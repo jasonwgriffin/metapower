397c6d3fa7cb1accddba018d51cf77ffe83fe6a4
jasonwgriffin <jxg569@psu.edu>
2020-04-15 12:35:54 -0400

    check

---
+++ R/plot_modpower.R
@@ -0,0 +1,18 @@
+#' Plot Power Curve for Categorical Moderator Analysis
+#'
+#' @export
+#'
+#' @param modpower This should be an "modpower" object
+#'
+#' @return Power curve plot for the user specified input parameters
+#'
+
+plot_modpower <- function(modpower){
+
+  if(class(modpower) != "modpower")
+    stop("Object must be of class: modpower")
+
+
+
+
+}
