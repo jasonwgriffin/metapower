789f203218a8d8ac3f4bbbfaf95853644de7205b
jasonwgriffin <jxg569@psu.edu>
2020-04-15 13:27:59 -0400

    fix typos in xlab and ggtitle

--- R/homogen_power_plot.R
+++ R/homogen_power_plot.R
@@ -10,8 +10,6 @@ homogen_power_plot <- function(obj){
 
   p_aes <- list(geom_line(size = 1),
                 scale_y_continuous(limits =c(0,1), breaks = c(0,.25,.5,.75,1)),
-                #geom_point(aes(x = obj$k, y = obj$power), shape = 21, color = "black", fill = "red", size = 3),
-                xlab("Standard deviation"),
                 ylab("Power"),
                 theme_classic(),
                 theme(legend.position = c(1,0),
@@ -27,14 +25,19 @@ homogen_power_plot <- function(obj){
         tidyr::pivot_longer(-"k_v", names_to = "power_type", values_to = "power")
 
       fixed_plot <- ggplot(fix_dat, aes(x = .data$SD, y = .data$fixed_power, linetype = .data$k_v)) +
-        geom_line(size = 1)+
+        geom_line(size = 1) +
         scale_x_continuous(limits = c(0, 6), breaks = c(seq(1:6))) +
+        ggtitle("Fixed-Effects Model") +
+        xlab("Standard Deviation") +
+        scale_linetype_discrete(name = "Number of Studies") +
         p_aes
 
       random_plot <- ggplot(rand_dat, aes(x = .data$k_v, y = .data$power, color = .data$power_type)) +
         geom_line(size = 1)+
         scale_x_continuous(limits = c(2,max(obj$homo_range$k_v)), breaks = c(seq(2,max(obj$homo_range$k_v),by = round(max(obj$homo_range$k_v)*.10,0)))) +
         p_aes +
+        ggtitle("Random-Effects Model") +
+        xlab("Number of Studies") +
         scale_color_manual(name = "Heterogeneity",
                            labels = c("Large", "Moderate", "Low"),
                            values = c("red","blue","green"))
