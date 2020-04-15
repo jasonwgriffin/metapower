73c0d5babf1486cec37f51b79d5b22c497082be4
jasonwgriffin <jxg569@psu.edu>
2020-04-15 13:22:01 -0400

    print spacing

--- R/print.mpower.R
+++ R/print.mpower.R
@@ -12,14 +12,14 @@ print.mpower <- function(x,...) {
     cat(" Estimated Power: Mean Effect Size \n\n")
 
     cat(" Fixed-Effects Model                            ", x$power$fixed_power, "\n")
-    cat(" Random-Effects Model (Small Heterogenity):       ", x$power$random_power_s, "\n")
+    cat(" Random-Effects Model (Small Heterogenity):     ", x$power$random_power_s, "\n")
     cat(" Random-Effects Model (Moderate Heterogeneity): ", x$power$random_power_m, "\n")
     cat(" Random-Effects Model (Large Heterogeneity):    ", x$power$random_power_l, "\n\n")
 
     cat(" Estimated Power: Test of Homogeneity \n\n")
 
     cat(" Fixed-Efects Model                             ", x$homo_power$fixed_power, "\n")
-    cat(" Random-Effects Model (Small Heterogeneity):      ", x$homo_power$random_power_s, "\n")
+    cat(" Random-Effects Model (Small Heterogeneity):    ", x$homo_power$random_power_s, "\n")
     cat(" Random-Effects Model (Moderate Heterogeneity): ", x$homo_power$random_power_m, "\n")
     cat(" Random-Effects Model (Large Heterogeneity):    ", x$homo_power$random_power_l, "\n")
 
