352d81e73e1961e15274e7c00ac178957703be40
jasonwgriffin <jxg569@psu.edu>
2020-04-20 10:48:47 -0400

    added unit tests (computation validation) for moderator analysis

--- tests/testthat/test-published-calc.R
+++ tests/testthat/test-published-calc.R
@@ -31,4 +31,34 @@ test_that("Check power test from Pigott 2012, page 43", {
                             k = 10,
                             es_type = "d",
                             test_type = "one-tailed")$power$random_power_l,2), .79)
+  ## moderator analysis
+
+  ## Page 71 - fixed power between study
+  expect_equal(round(mod_power(n_groups = 3,
+                               effect_sizes = c(.1,.2,.6),
+                               sample_size = 15,
+                               k = 15,
+                               es_type = "Correlation",
+                               test_type = "one-tailed")$mod_power$fixed_power_b,2), .83)
+  ## page 74 - fixed power within study
+  expect_equal(round(mod_power(n_groups = 3,
+                               effect_sizes = c(.1,.2,.6),
+                               sample_size = 15,
+                               k = 15,
+                               es_type = "Correlation",
+                               sd_within = c(1,1,4),
+                               test_type = "one-tailed")$mod_power$fixed_power_w,2), .83)
+
+  ## Page 78 - random power (small heterogeneity)
+  expect_equal(round(mod_power(n_groups = 3,
+                               effect_sizes = c(.1,.2,.6),
+                               sample_size = 15,
+                               k = 15,
+                               es_type = "Correlation",
+                               sd_within = c(1,1,4),
+                               test_type = "one-tailed")$mod_power$random_power_s,2), .05)
+
+
+
+
 })
