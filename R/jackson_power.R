d9433d452fad8e0da85a590bed0177e9e077889e
Jason W. Griffin <jxg569@psu.edu>
2020-08-19 13:23:14 -0400

    remove sd arg; doesnt need to be explicitly specified here

--- R/jackson_power.R
+++ R/jackson_power.R
@@ -1,9 +1,7 @@
-
-jackson_power <- function(k, es, sd, i2){
+jackson_power <- function(k, es, i2){
   Z <- qnorm(0.975) ## alpha value
   t <- Z
-  ncp <- es*sqrt(k)/sd
+  ncp <- es*sqrt(k)/sqrt(variance)
   random_power <- 1 - (CDF(Z, k, ncp, i2) - CDF(-Z, k, ncp, i2))
   return(random_power)
 }
-
