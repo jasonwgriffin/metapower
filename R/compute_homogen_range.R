084fea05aeb6df28d8564d6f3274c427e7303820
Jason W. Griffin <jxg569@psu.edu>
2020-08-20 11:56:28 -0400

    Revert "f"
    This reverts commit 50387591a2952b959b8880b3c15fd1dc09eb94e6.

---
+++ R/compute_homogen_range.R
@@ -0,0 +1,30 @@
+compute_homogen_range <- function(effect_size, sample_size, k, es_type, test_type, p, sd, con_table){
+
+  if(missing(con_table))
+    con_table = NA
+
+  range_factor <- 5
+
+  if(es_type == "d" | es_type == "Correlation"){
+    homo_range <- data.frame(SD = rep(seq(0,6), each = (k*range_factor-1)),
+                      k_v = rep(seq(2,range_factor*k),times = 7),
+                      es_v = effect_size,
+                      n_v = sample_size,
+                      variance = mapply(compute_variance, sample_size, effect_size, es_type))
+    homo_range <- cbind.data.frame(homo_range, as.data.frame((t(mapply(homogen_power, homo_range$es_v, homo_range$variance, homo_range$n_v, homo_range$k_v, es_type, test_type, p, homo_range$SD)))) %>%
+                                     dplyr::mutate_all(as.numeric))
+    return(homo_range)
+
+    }else if (es_type =="OR"){
+
+    variance <- compute_variance(sample_size, effect_size, es_type, con_table)
+    homo_range <- data.frame(SD = rep(seq(0,6), each = (k*range_factor-1)),
+                         k_v = rep(seq(2,range_factor*k),times = 7),
+                         es_v = effect_size,
+                         n_v = sample_size,
+                         variance = variance)
+    homo_range <- cbind.data.frame(homo_range, as.data.frame((t(mapply(homogen_power, homo_range$es_v, homo_range$variance, homo_range$n_v, homo_range$k_v, es_type, test_type, p, homo_range$SD)))) %>%
+                                     dplyr::mutate_all(as.numeric))
+    return(homo_range)
+  }
+}
