53a7a1aaeaf95b3b1dda843ac982503bc97f0d57
Jason W. Griffin <jxg569@psu.edu>
2020-08-20 11:58:35 -0400

    Revert "new function for jackson & turner (2017) analytical approach to RE meta power"
    This reverts commit cae8cf4c95d37698f5754a3cf2c8883bcf17448a.

--- R/jackson_power.R
+++
@@ -1,9 +0,0 @@
-
-jackson_power <- function(k, es, sd, i2){
-  Z <- qnorm(0.975) ## alpha value
-  t <- Z
-  ncp <- es*sqrt(k)/sd
-  random_power <- 1 - (CDF(Z, k, ncp, i2) - CDF(-Z, k, ncp, i2))
-  return(random_power)
-}
-
