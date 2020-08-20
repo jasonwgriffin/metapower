90e0247f756a5c61fb60900425a01769893c7f8f
Jason W. Griffin <jxg569@psu.edu>
2020-08-20 11:58:31 -0400

    Revert "remove sd arg; doesnt need to be explicitly specified here"
    This reverts commit d9433d452fad8e0da85a590bed0177e9e077889e.

--- R/jackson_power.R
+++ R/jackson_power.R
@@ -1,7 +1,9 @@
-jackson_power <- function(k, es, i2){
+
+jackson_power <- function(k, es, sd, i2){
   Z <- qnorm(0.975) ## alpha value
   t <- Z
-  ncp <- es*sqrt(k)/sqrt(variance)
+  ncp <- es*sqrt(k)/sd
   random_power <- 1 - (CDF(Z, k, ncp, i2) - CDF(-Z, k, ncp, i2))
   return(random_power)
 }
+
