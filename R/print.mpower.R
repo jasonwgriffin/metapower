f0f3fc426c1274820a3f83de6c8137fe25ac75f0
Jason W. Griffin <jxg569@psu.edu>
2020-08-24 10:04:06 -0400

    print.mpower removing homogen power

--- R/print.mpower.R
+++ R/print.mpower.R
@@ -19,11 +19,11 @@ print.mpower <- function(x,...) {
     cat(" Random-Effects Model (i2 =  75% ):     ", x$power$random_power_75, "\n")
     cat(" Random-Effects Model (i2 = 100% )      ", x$power$random_power_100, "\n\n")
 
-    cat(" Estimated Power: Test of Homogeneity \n\n")
+    #cat(" Estimated Power: Test of Homogeneity \n\n")
 
-    cat(" Fixed-Efects Model                             ", x$homo_power$fixed_power, "\n")
-    cat(" Random-Effects Model (Small Heterogeneity):    ", x$homo_power$random_power_s, "\n")
-    cat(" Random-Effects Model (Moderate Heterogeneity): ", x$homo_power$random_power_m, "\n")
-    cat(" Random-Effects Model (Large Heterogeneity):    ", x$homo_power$random_power_l, "\n")
+    #cat(" Fixed-Efects Model                             ", x$homo_power$fixed_power, "\n")
+    #cat(" Random-Effects Model (Small Heterogeneity):    ", x$homo_power$random_power_s, "\n")
+    #cat(" Random-Effects Model (Moderate Heterogeneity): ", x$homo_power$random_power_m, "\n")
+    #cat(" Random-Effects Model (Large Heterogeneity):    ", x$homo_power$random_power_l, "\n")
 
 }
