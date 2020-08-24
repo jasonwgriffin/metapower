5db4ef81934744aeb61b003e7e4b7dc841c5e9cf
Jason W. Griffin <jxg569@psu.edu>
2020-08-24 10:42:35 -0400

    update print.mpower

--- R/print.mpower.R
+++ R/print.mpower.R
@@ -11,17 +11,16 @@ print.mpower <- function(x,...) {
 
     cat(" Estimated Power: Mean Effect Size \n\n")
 
-    cat(" Fixed-Effects Model                    ", x$power$fixed_power, "\n")
-    cat(" Random-Effects Model (i2 =  XX% ):     ", x$power$random_power, "\n")
-    cat(" Random-Effects Model (i2 =  00% ):     ", x$power$random_power_0, "\n")
-    cat(" Random-Effects Model (i2 =  25% ):     ", x$power$random_power_25, "\n")
-    cat(" Random-Effects Model (i2 =  50% ):     ", x$power$random_power_50, "\n")
-    cat(" Random-Effects Model (i2 =  75% ):     ", x$power$random_power_75, "\n")
-    cat(" Random-Effects Model (i2 = 100% )      ", x$power$random_power_100, "\n\n")
+    cat(" Fixed-Effects Model               ", x$power$fixed_power, "\n")
+    cat(" Random-Effects Model (i2 = ",round(x$i2*100,2),"% ):  ", x$power$random_power, "\n", sep = "")
+    cat(" Random-Effects Model (i2 =  0% ): ", x$power$random_power_0, "\n")
+    cat(" Random-Effects Model (i2 = 25% ): ", x$power$random_power_25, "\n")
+    cat(" Random-Effects Model (i2 = 50% ): ", x$power$random_power_50, "\n")
+    cat(" Random-Effects Model (i2 = 75% ): ", x$power$random_power_75, "\n")
 
     #cat(" Estimated Power: Test of Homogeneity \n\n")
 
-    #cat(" Fixed-Efects Model                             ", x$homo_power$fixed_power, "\n")
+    #cat(" Fixed-Effects Model                             ", x$homo_power$fixed_power, "\n")
     #cat(" Random-Effects Model (Small Heterogeneity):    ", x$homo_power$random_power_s, "\n")
     #cat(" Random-Effects Model (Moderate Heterogeneity): ", x$homo_power$random_power_m, "\n")
     #cat(" Random-Effects Model (Large Heterogeneity):    ", x$homo_power$random_power_l, "\n")
