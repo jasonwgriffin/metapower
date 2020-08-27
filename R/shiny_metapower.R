294934412e4c7135a99c75f51ab40655db39aa5e
Jason W. Griffin <jxg569@psu.edu>
2020-08-27 01:32:21 -0400

    re-named shiny app folder

--- R/shiny_metapower.R
+++ R/shiny_metapower.R
@@ -4,7 +4,7 @@
 #'
 
 shiny_metapower <- function() {
-  appDir <- system.file("shiny", package = "metapower")
+  appDir <- system.file("shiny_metapower", package = "metapower")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `metapower`.", call. = FALSE)
   }
