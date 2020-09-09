93886fd53b2af5e9803a968558ff0dde1321c8ae
Jason W. Griffin <jxg569@psu.edu>
2020-09-09 12:52:57 -0400

    add s3 summary methods

--- R/print.mpower.R
+++ R/print.mpower.R
@@ -1,12 +1,11 @@
 #' @export
 
 print.mpower <- function(x,...) {
-
     cat("\n Power Analysis for Meta-analysis \n\n")
 
     cat(" Expected Effect Size:             ", x$effect_size, "\n")
     cat(" Expected Sample Size (per group): ", x$sample_size, "\n")
-    cat(" Expected Number of Studies:       ", x$k, "\n")
+    cat(" Expected Number of Studies:       ", x$k, "\n\n")
 
     cat(" Estimated Power: Mean Effect Size \n\n")
 
@@ -15,5 +14,4 @@ print.mpower <- function(x,...) {
     cat(" Random-Effects Model (i2 = 25% ): ", x$power$random_power_25, "\n")
     cat(" Random-Effects Model (i2 = 50% ): ", x$power$random_power_50, "\n")
     cat(" Random-Effects Model (i2 = 75% ): ", x$power$random_power_75, "\n")
-
 }
---
+++ R/summary.mpower.R
@@ -0,0 +1,17 @@
+#' @export
+
+summary.mpower <- function(x,...) {
+  cat("\n Power Analysis for Meta-analysis \n\n")
+
+  cat(" Expected Effect Size:             ", x$effect_size, "\n")
+  cat(" Expected Sample Size (per group): ", x$sample_size, "\n")
+  cat(" Expected Number of Studies:       ", x$k, "\n\n")
+
+  cat(" Estimated Power: Mean Effect Size \n\n")
+
+  cat(" Fixed-Effects Model               ", x$power$fixed_power, "\n")
+  cat(" Random-Effects Model (i2 =  0% ): ", x$power$random_power_0, "\n")
+  cat(" Random-Effects Model (i2 = 25% ): ", x$power$random_power_25, "\n")
+  cat(" Random-Effects Model (i2 = 50% ): ", x$power$random_power_50, "\n")
+  cat(" Random-Effects Model (i2 = 75% ): ", x$power$random_power_75, "\n")
+}
