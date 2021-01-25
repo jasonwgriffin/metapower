12bda161e2ab068d219df2fe76e8db8747b3b4cf
Jason W. Griffin <jxg569@psu.edu>
2021-01-25 07:51:28 -0500

    change plotter to explicitly plot models with user-specified i2

--- R/plot_mpower.R
+++ R/plot_mpower.R
@@ -31,26 +31,20 @@ plot_mpower <- function(obj){
     dplyr::filter(obj$power_range$es_v == obj$effect_size) %>%
     dplyr::select(c("k_v", dplyr::starts_with("random"))) %>%
     tidyr::pivot_longer(-"k_v", names_to = "power_type", values_to = "power")
-  rand_dat$power_type <- factor(rand_dat$power_type, levels = c("random_power", "random_power_0", "random_power_25", "random_power_50", "random_power_75", "random_power_100"))
   obj$power_range$es_v <- as.factor(obj$power_range$es_v)
 
 
   # fixed plot
-  fixed_plot <- ggplot(obj$power_range, aes(x = .data$k_v, y = .data$fixed_power, linetype = .data$es_v)) +
+  fixed_plot <- ggplot(obj$power_range, aes(x = .data$k_v, y = .data$fixed_power, color = .data$es_v)) +
       p_aes +
       ggtitle("Fixed-Effects Model") +
-      labs(linetype = "Effect Size")
+      labs(color = "Effect Size")
 
   # random plot
-  random_plot <- ggplot(rand_dat, aes(x = .data$k_v, y = .data$power, group = .data$power_type, color = .data$power_type)) +
-      p_aes +
-      ggtitle("Random-Effects Model") +
-      scale_color_manual(name = "Heterogeneity",
-                         labels = c(bquote(I^2 == "0%"),
-                                    bquote(I^2 == "25%"),
-                                    bquote(I^2 == "50%"),
-                                    bquote(I^2 == "75%")),
-                         values = rev(c("#993366", "#9900CC", "#9966FF", "#6699FF", "#33CCCC")))
+  random_plot <- ggplot(rand_dat, aes(x = .data$k_v, y = .data$power)) +
+    p_aes +
+    geom_point(aes(x = obj$k, y = obj$power$random_power), size = 4, shape = 21, fill = "white", stroke = 1) +
+    ggtitle(paste0("Random-Effects Model (I^2 = ", round(obj$i2*100,2),"%)"))
 
   #arrange for plotting
   p <- cowplot::plot_grid(fixed_plot, random_plot, ncol = 1)
